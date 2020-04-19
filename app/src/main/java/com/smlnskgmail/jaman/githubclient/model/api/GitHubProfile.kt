6ff5638a20fd59c6e6a0423c3aa45707071e5f7e
Artem <jaman.smlnsk@gmail.com>
2020-04-19 18:47:08 +0300

    2020-04-19 Version 1.0.0: Updated profile structure

--- app/src/main/java/com/smlnskgmail/jaman/githubclient/model/api/GitHubProfile.kt
+++ app/src/main/java/com/smlnskgmail/jaman/githubclient/model/api/GitHubProfile.kt
@@ -3,8 +3,8 @@ package com.smlnskgmail.jaman.githubclient.model.api
 import android.graphics.Bitmap
 
 data class GitHubProfile(
+    val fullName: String,
     val name: String,
-    val email: String,
     val countOfPublicRepositories: Int,
     val countOfPublicGists: Int,
     val photo: Bitmap
